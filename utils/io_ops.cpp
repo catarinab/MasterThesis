2e719d18549d6488160f61193757bfb5a708a313
catarinab <catarina.c.bento@tecnico.ulisboa.pt>
2023-12-06 18:45:30 +0000

    parallelize build matrix

--- utils/io_ops.cpp
+++ utils/io_ops.cpp
@@ -15,29 +15,43 @@ void initRows(int rows, int cols) {
     rowValues.resize(rows);
 }
 
+void processLine(string line) {
+    stringstream ss(line);
+    for(int i = 0; i < 3; i++) {
+        getline(ss, line, ' ');
+        values[i] = stod(line);
+    }
+    rowValues[(int) values[0] - 1]
+            .push_back(SparseTriplet( (int) values[0] - 1, (int) values[1] - 1, values[2]));
+}
+
 vector<vector<SparseTriplet>> readFile_mtx(string inputFile, int * rows, int * cols, int * nz) {
     //paralelizar ?
+    
     ifstream file(inputFile);
     string line;
     bool isDefined = false;
     while (getline(file, line)) {
         if(line[0] == '%') continue;
-        stringstream ss(line);
-        for(int i = 0; i < 3; i++) {
-            getline(ss, line, ' ');
-            values[i] = stod(line);
-        }
         if(!isDefined) {
+            stringstream ss(line);
+            for(int i = 0; i < 3; i++) {
+                getline(ss, line, ' ');
+                values[i] = stod(line);
+            }
             *rows = values[0];
             *cols = values[1];
             *nz = values[2];
             initRows(values[0], values[1]);
             isDefined = true;
-        } else
-            rowValues[(int) values[0] - 1]
-                .push_back(SparseTriplet( (int) values[0] - 1, (int) values[1] - 1, values[2]));
+        }
+        else {
+            #pragma omp task firstprivate(line)
+                processLine(line);
+        }
+    } 
         
-    }
+    #pragma omp taskwait
     file.close();
 
     return rowValues;
