d040cef4e675186362318cb9b5768cac4ef61114
catarinab <catarina.c.bento@tecnico.ulisboa.pt>
2024-04-06 16:09:54 +0100

    Eigen Schur-Parlett Algorithm

---
+++ utils/calculate-MLF-Eigen.cpp
@@ -0,0 +1,54 @@
+#include <cmath>
+#include <vector>
+#include <complex>
+#include <functional>
+#include <iostream>
+#include <complex>
+#include <unsupported/Eigen/MatrixFunctions>
+#include <Eigen/Dense>
+
+#include "headers/utils.hpp"
+#include "headers/Evaluate-Single-ML.hpp"
+#include "mkl.h"
+
+using namespace std;
+
+/*
+Algorithm based on the paper "Computing the matrix Mittag–Leffler function with applications to fractional calculus"
+by Roberto Garrappa and Marina Popolizio
+*/
+
+double alpha;
+double betaVal;
+
+complex<double> evaluateSingleWrapper(complex<double> z, int k) {
+    return evaluateSingle(z, alpha, betaVal, k);
+}
+
+
+complex<double> * calculate_MLF(complex<double> * T, double alphaInput, double betaInput, int size) {
+    alpha = alphaInput;
+    betaVal = betaInput;
+
+    cout << "calculating with eigen" << endl;
+
+    Eigen::MatrixXcd A(size, size);
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            A(i,j) = T[i*size + j];
+        }
+    }
+
+    Eigen::MatrixXcd result = A.matrixFunction(evaluateSingleWrapper);
+
+    auto * resultArray = (complex<double> *) malloc(size * size * sizeof(complex<double>));
+
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            resultArray[i*size + j] = {result(i,j).real(), result(i,j).imag()};
+        }
+    }
+
+    return resultArray;
+}
+
