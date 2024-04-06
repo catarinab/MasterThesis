23c2b078d24d5b4b92f0332e2f0f868b2b4a71a2
catarinab <catarina.c.bento@tecnico.ulisboa.pt>
2024-04-06 15:59:41 +0100

    Reorganize ML evaluation

---
+++ utils/headers/Evaluate-Single-ML.hpp
@@ -0,0 +1,8 @@
+#ifndef MLF_LTI_HPP
+#define MLF_LTI_HPP
+
+#include <complex>
+
+std::complex<double> evaluateSingle(std::complex<double> tVal, double alpha, double beta, int k);
+
+#endif
--- utils/headers/MLF-LTI.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef MLF_LTI_HPP
-#define MLF_LTI_HPP
-
-#include <complex>
-
-complex<double> LTI(complex<double> lambda, double alpha, double beta, int kd);
-
-#endif
