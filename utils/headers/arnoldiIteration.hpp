9ff4dffe7a3b8d1234ded2a932cb9aa82600a8ea
catarinab <catarina.c.bento@tecnico.ulisboa.pt>
2024-04-16 09:59:47 +0100

    Add hint to MKL matrix vector multiplication, improved performance

--- utils/headers/arnoldiIteration.hpp
+++ utils/headers/arnoldiIteration.hpp
@@ -6,7 +6,7 @@
 #include "dense_vector.hpp"
 
 
-int arnoldiIteration(const csr_matrix& A, dense_vector b, int k_total, int m, int me, int nprocs, dense_matrix * V,
-                        dense_matrix * H);
+int arnoldiIteration(csr_matrix A, dense_vector b, int k_total, int m, int me, int nprocs, dense_matrix * V,
+                     dense_matrix * H);
 
 #endif // ARNOLDIITERATION_HPP
