d5693e650f26b8dd72a6cd86931fe55d8bfd86fe
Catarina Bento <catarina.c.bento@tecnico.ulisboa.pt>
2024-05-01 17:05:12 +0100

    script for testing

--- MLF/script.sh
+++ MLF/script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-for (( i=50; i <= 610; i += 20 ))
+for (( t=64; t >= 64; t /= 2 ))
 do
-    echo "doing size $i"
-    ./test.out "${i}"; cat "krylov/${i}/cpp.txt" >> "krylov/resultados.txt"
+    for (( i=50; i <= 610; i += 20 ))
+    do
+        export OMP_NUM_THREADS=$t
+        export MKL_NUM_THREADS=$t
+        echo ${OMP_NUM_THREADS}
+        echo "doing size $i"
+        ./mlf-shared.out -k "${i}" >> "newpar/2kkresultados-${t}";
+    done
+    cat "newpar/2kkresultados-${t}"
 done
