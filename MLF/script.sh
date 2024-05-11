98071dd29e312a771d75be833992f4a0bf80412d
catarinab <catarina.c.bento@tecnico.ulisboa.pt>
2024-05-11 12:09:04 +0100

    arnoldi testing script

--- MLF/script.sh
+++ MLF/script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-for (( t=64; t >= 64; t /= 2 ))
+for (( t=32; t >= 2; t /= 2 ))
 do
-    for (( i=50; i <= 610; i += 20 ))
+    export OMP_NUM_THREADS=$t
+    export MKL_NUM_THREADS=$t
+    echo "${OMP_NUM_THREADS}"
+    echo "${MKL_NUM_THREADS}"
+    for (( i=610; i <= 610; i += 20 ))
     do
-        export OMP_NUM_THREADS=$t
-        export MKL_NUM_THREADS=$t
-        echo ${OMP_NUM_THREADS}
         echo "doing size $i"
         ./mlf-shared.out -k "${i}" >> "newpar/2kkresultados-${t}";
     done
