4cf4bdebab103132fed001842f44a35456d6928c
catarinab <catarina.c.bento@tecnico.ulisboa.pt>
2024-06-13 16:42:32 +0100

    Experimenting with new Distributed MLF

--- utils/arnoldi_iteration-new.cpp
+++ utils/arnoldi_iteration-new.cpp
@@ -2,7 +2,6 @@
 
 #include "headers/arnoldi_iteration.hpp"
 #include "headers/distr_mtx_ops.hpp"
-#include "headers/mtx_ops_mkl.hpp"
 
 /*  Parameters
     ----------
@@ -23,43 +22,45 @@ int arnoldiIteration(const csr_matrix& A, dense_vector& initVec, int k_total, in
     V->setCol(0, initVec);
 
     //auxiliary
-    dense_vector privW(counts[me]);
-    dense_vector w(m);
+    auto * privW = (double *) malloc(counts[me] * sizeof(double));
+    auto * w = (double *) malloc(m * sizeof(double));
     double * vCol;
 
     for(int k = 1; k < k_total + 1; k++) {
 
         V->getCol(k-1, &vCol);
         mkl_sparse_d_mv(SPARSE_OPERATION_NON_TRANSPOSE, 1.0, A.getMKLSparseMatrix(), A.getMKLDescription(),
-                        vCol, 0.0, privW.values.data());
+                        vCol, 0.0, privW);
 
         for(int j = 0; j < k; j++) {
             V->getCol(j, &vCol, displs[me]);
 
-            double temp = cblas_ddot(counts[me], privW.values.data(), 1, vCol, 1);
+            double temp = cblas_ddot(counts[me], privW, 1, vCol, 1);
             MPI_Allreduce(&temp, &dotProd, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
-            cblas_daxpy(counts[me], -dotProd, vCol, 1, privW.values.data(), 1);
+            cblas_daxpy(counts[me], -dotProd, vCol, 1, privW, 1);
 
             H->setValue(j, k-1, dotProd);
         }
 
-        MPI_Allgatherv(&privW.values[0], helpSize, MPI_DOUBLE, &w.values[0], counts, displs, MPI_DOUBLE, MPI_COMM_WORLD);
+        MPI_Allgatherv(privW, helpSize, MPI_DOUBLE, w, counts, displs, MPI_DOUBLE, MPI_COMM_WORLD);
 
         if(k == k_total) break;
 
-        double wNorm = w.getNorm2();
+        double wNorm = cblas_dnrm2(m, w, 1);
 
-        if(wNorm < 1e-52)
-            return k;
+        if(wNorm < 1e-52) break;
 
         H->setValue(k, k - 1, wNorm);
         V->getCol(k, &vCol);
         #pragma omp parallel for
         for(int i = 0; i < m; i++){
-            vCol[i] = w.values[i] / wNorm;
+            vCol[i] = w[i] / wNorm;
         }
     }
+
+    free(w);
+    free(privW);
 
     return k_total;
 }
