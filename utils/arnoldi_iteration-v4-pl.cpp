f686a0222181bf5dca522df88e33514b29bddf7e
catarinab <catarina.c.bento@tecnico.ulisboa.pt>
2024-06-29 15:07:45 +0100

    parallelization of v4 distributed arnoldi iteration

--- utils/arnoldi_iteration-v4-pl.cpp
+++ utils/arnoldi_iteration-v4-pl.cpp
@@ -26,19 +26,19 @@
  * */
 
 //Calculate the approximation of MLF(A)*b
-dense_vector getApproximation(dense_matrix& V, const dense_matrix& mlfH, double betaVal) {
+void getApproximation(dense_matrix& V, const dense_matrix& mlfH, double betaVal, dense_vector & res) {
     if(betaVal != 1)
         V = V * betaVal;
 
-    return denseMatrixMult(V, mlfH).getCol(0);
+    denseMatrixMult(V, mlfH).getCol(0, &res);
 }
 
 dense_vector restartedArnoldiIteration_MLF(const csr_matrix& A, dense_vector& initVec, int k_total, int m, int me, dense_matrix * V,
                                            dense_matrix * H, double alpha, double beta, double betaNormB, int l) {
     int k = 0;
-
     dense_vector b(m);
     dense_vector temp(counts[me]);
+    dense_vector res(counts[me]);
 
 
     while(true) {
@@ -53,10 +53,11 @@ dense_vector restartedArnoldiIteration_MLF(const csr_matrix& A, dense_vector& in
         if(me == 0) {
             mlfH = calculate_MLF((double *) H->getDataPointer(), alpha, beta, size);
         }
+        k += size;
 
         MPI_Bcast(mlfH.getValues(), size * size, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
 
-        dense_vector res = getApproximation(*V, mlfH, betaNormB);
+        getApproximation(*V, mlfH, betaNormB, res);
 
         cblas_daxpy(counts[me], 1, res.values.data(), 1, temp.values.data(), 1);
 
@@ -78,7 +79,8 @@ int arnoldiIteration(const csr_matrix& A, dense_vector& initVec, int k_total, in
     memcpy(z, initVec.values.data(), m * sizeof(double));
     dense_matrix Z(counts[me], k_total + l + 2);
     dense_matrix G(counts[me], k_total + l + 2);
-    double * vCol, * zCol, *xZcol;
+    double * vCol, *xZcol;
+    double * zCol;
     auto vValVec = new double[counts[me]]();
     auto dotProds = new double[k_total + l + 2];
     MPI_Request r1, r2;
@@ -90,7 +92,6 @@ int arnoldiIteration(const csr_matrix& A, dense_vector& initVec, int k_total, in
     int i;
 
     for(i = 0; i <= k_total + l; i++) {
-        //fprintf(stderr, "Iteration: %d\n", i);
         Z.getCol(i + 1, &zCol);
         mkl_sparse_d_mv(SPARSE_OPERATION_NON_TRANSPOSE, 1.0, A.getMKLSparseMatrix(), A.getMKLDescription(),
                         z, 0.0, zCol);
@@ -99,7 +100,6 @@ int arnoldiIteration(const csr_matrix& A, dense_vector& initVec, int k_total, in
         if(i > 0) {
             MPI_Wait(&r2, MPI_STATUS_IGNORE);
             for (int j = 0; j <= i; j++) {
-                //fprintf(stderr, "Setting G(%d, %d) to %f\n", j, i, dotProds[j]);
                 G.setValue(j, i, dotProds[j]);
             }
         }
@@ -112,111 +112,101 @@ int arnoldiIteration(const csr_matrix& A, dense_vector& initVec, int k_total, in
                     continue;
                 sumH1 = 0;
                 for (int k = 0; k <= j - 1; k++) {
-                    //fprintf(stderr, "getting G(%d, %d) and G(%d, %d)\n", k, j, k, a + 1);
                     sumH1 += G.getValue(k, j) * G.getValue(k, a + 1);
                 }
-                //fprintf(stderr, "Getting G(%d, %d) ( = %f) \n", j, j, G.getValue(j, j));
                 G.setValue(j, a + 1, (G.getValue(j, a + 1) - sumH1) / G.getValue(j, j));
-                //fprintf(stderr, "Setting G(%d, %d) to %f\n", j, a + 1, G.getValue(j, a + 1));
             }
 
 
             for (int k = 0; k <= a; k++) {
-                //fprintf(stderr, "getting G(%d, %d) ( = %f) \n", k, a + 1, G.getValue(k, a + 1));
                 sumH2 += pow(G.getValue(k, a + 1), 2);
             }
             double gVal = G.getValue(a + 1, a + 1);
+
             //Check for breakdown and restart or reorthogonalize if necessary
             if (gVal - sumH2 < 0) {
-                //fprintf(stderr, "Breakdown at k = %d\n", i);
                 break;
             }
 
             G.setValue(a + 1, a + 1, sqrt(gVal - sumH2));
 
-            if (a < l) {
-                for (int j = 0; j <= a; j++) {
-                    sumH1 = 0;
-                    for (int k = 0; k <= a - 1; k++) {
-                        //fprintf(stderr, "getting H(%d, %d) and G(%d, %d)\n", j, k, k, a);
-                        sumH1 += H->getValue(j, k) * G.getValue(k, a);
-                    }
-                    //fprintf(stderr, "getting G(%d, %d), G(%d, %d) and G(%d, %d)\n", j, a+1, j, a, a,a);
-                    H->setValue(j, a, (G.getValue(j, a + 1) + G.getValue(j, a) - sumH1) / G.getValue(a, a));
-                    //fprintf(stderr, "Setting H(%d, %d) to %f\n", j, a, (G.getValue(j, a + 1) + G.getValue(j, a) - sumH1) / G.getValue(a, a));
-                }
-                //fprintf(stderr, "getting G(%d, %d) and G(%d, %d)\n", a + 1, a + 1, a, a);
-                H->setValue(a + 1, a, G.getValue(a + 1, a + 1) / G.getValue(a, a));
-                //fprintf(stderr, "Setting H(%d, %d) to %f\n", a + 1, a, H->getValue(a + 1, a));
-            }
-            else {
-                for (int j = 0; j <= a; j++) {
-                    sumH1 = 0;
-                    sumH2 = 0;
-                    for (int k = 0; k <= a + 1 - l; k++) {
-                        //fprintf(stderr, "getting G(%d, %d) and H(%d, %d)\n", j, k + l, k, a - l);
-                        sumH1 += G.getValue(j, k + l) * H->getValue(k, a - l);
-                    }
-                    for (int k = j - 1; k <= a - 1; k++) {
-                        //fprintf(stderr, "getting H(%d, %d) and G(%d, %d)\n", j, k, k, a);
-                        sumH2 += H->getValue(j, k) * G.getValue(k, a);
-                    }
-                    //fprintf(stderr, "getting G(%d, %d)\n", a, a);
-                    H->setValue(j, a, (sumH1 - sumH2) / G.getValue(a, a));
-                    //fprintf(stderr, "Setting H(%d, %d) to %f\n", j, a, H->getValue(j, a));
-                }
-                //fprintf(stderr, "getting G(%d, %d), G(%d, %d) and H(%d, %d)\n", a + 1, a + 1, a, a, a + 1 - l, a - l);
-                H->setValue(a + 1, a, (G.getValue(a + 1, a + 1) * H->getValue(a + 1 - l, a - l)) / G.getValue(a, a));
-                //fprintf(stderr, "Setting H(%d, %d) to %f\n", a + 1, a, H->getValue(a + 1, a));
-
-            }
-
-            memset(vValVec, 0, counts[me] * sizeof(double));
-            //#pragma omp for reduction(+:vValVec[:counts[me]]) private(vCol)
-            for (int j = 0; j <= a - 1; j++) {
-                V->getCol(j, &vCol);
-                gVal = G.getValue(j, a);
-                if(gVal == 0)
-                    continue;
-                //fprintf(stderr, "Getting G(%d, %d)\n", j, a);
-                for (int idx = 0; idx < counts[me]; idx++) {
-                    vValVec[idx] += vCol[idx] * gVal;
-                }
-            }
-
-            V->getCol(a, &vCol);
+            //porque ???
             Z.getCol(a, &zCol);
-            gVal = G.getValue(a, a);
-            //fprintf(stderr, "Getting G(%d, %d) = %f\n", a, a, gVal);
-            //fprintf(stderr, "gVal: %f\n", gVal);
-            //#pragma omp for
-            for (int idx = 0; idx < counts[me]; idx++) {
-                vCol[idx] = (zCol[idx] - vValVec[idx]) / gVal;
-            }
-            if(a > 0) {
-                memset(vValVec, 0, counts[me] * sizeof(double));
-                //#pragma omp for reduction(+:vValVec[:counts[me]]) private(vCol)
+            #pragma omp parallel private(vCol, gVal) firstprivate(zCol) shared(vValVec, a, i)
+            {
+                if (a < l) {
+                    #pragma omp for private(sumH1)
+                    for (int j = 0; j <= a; j++) {
+                        sumH1 = 0;
+                        for (int k = 0; k <= a - 1; k++) {
+                            sumH1 += H->getValue(j, k) * G.getValue(k, a);
+                        }
+                        H->setValue(j, a, (G.getValue(j, a + 1) + G.getValue(j, a) - sumH1) / G.getValue(a, a));
+                    }
+                    #pragma omp single
+                        H->setValue(a + 1, a, G.getValue(a + 1, a + 1) / G.getValue(a, a));
+                } else {
+                    #pragma omp for private(sumH1, sumH2)
+                    for (int j = 0; j <= a; j++) {
+                        sumH1 = 0;
+                        sumH2 = 0;
+                        for (int k = 0; k <= a + 1 - l; k++) {
+                            sumH1 += G.getValue(j, k + l) * H->getValue(k, a - l);
+                        }
+                        for (int k = j - 1; k <= a - 1; k++) {
+                            sumH2 += H->getValue(j, k) * G.getValue(k, a);
+                        }
+                        H->setValue(j, a, (sumH1 - sumH2) / G.getValue(a, a));
+                    }
+                    #pragma omp single
+                        H->setValue(a + 1, a,
+                                (G.getValue(a + 1, a + 1) * H->getValue(a + 1 - l, a - l)) / G.getValue(a, a));
+
+                }
+
+                #pragma omp for reduction(+:vValVec[:counts[me]]) private(vCol, gVal)
                 for (int j = 0; j <= a - 1; j++) {
-                    Z.getCol(j + l, &zCol);
-                    double hVal = H->getValue(j, a - 1);
-                    if(hVal == 0)
+                    V->getCol(j, &vCol);
+                    gVal = G.getValue(j, a);
+                    if(gVal == 0)
                         continue;
-                    //fprintf(stderr, "Getting H(%d, %d)\n", j, a - 1);
                     for (int idx = 0; idx < counts[me]; idx++) {
-                        vValVec[idx] += zCol[idx] * hVal;
+                        vValVec[idx] += vCol[idx] * gVal;
                     }
                 }
 
-                Z.getCol(i + 1, &zCol);
-                //#pragma omp for
-                double hVal = H->getValue(a, a - 1);
-                //fprintf(stderr, "Getting H(%d, %d) = %f\n", a, a - 1, hVal);
-                //fprintf(stderr, "hVal: %f\n", hVal);
+                V->getCol(a, &vCol);
+                gVal = G.getValue(a, a);
+                #pragma omp for
                 for (int idx = 0; idx < counts[me]; idx++) {
-                    zCol[idx] = (zCol[idx] - vValVec[idx]) / hVal;
+                    vCol[idx] = (zCol[idx] - vValVec[idx]) / gVal;
+                    vValVec[idx] = 0;
+                }
+
+                if(a > 0) {
+                    #pragma omp for reduction(+:vValVec[:counts[me]]) private(zCol)
+                    for (int j = 0; j <= a - 1; j++) {
+                        Z.getCol(j + l, &zCol);
+                        double hVal = H->getValue(j, a - 1);
+                        if(hVal == 0)
+                            continue;
+                        for (int idx = 0; idx < counts[me]; idx++) {
+                            vValVec[idx] += zCol[idx] * hVal;
+                        }
+                    }
+
+                    Z.getCol(i + 1, &zCol);
+                    double hVal = H->getValue(a, a - 1);
+                    #pragma omp for
+                    for (int idx = 0; idx < counts[me]; idx++) {
+                        zCol[idx] = (zCol[idx] - vValVec[idx]) / hVal;
+                        vValVec[idx] = 0;
+                    }
                 }
             }
         }
+
+
         MPI_Iallgatherv(zCol, counts[me], MPI_DOUBLE, z, counts, displs, MPI_DOUBLE, MPI_COMM_WORLD, &r1);
 
         memset(dotProds, 0, (i + 2) * sizeof(double));
@@ -224,7 +214,6 @@ int arnoldiIteration(const csr_matrix& A, dense_vector& initVec, int k_total, in
         for (int j = 0; j <= a; j++) {
             V->getCol(j, &vCol);
             dotProds[j] = cblas_ddot(counts[me], zCol, 1, vCol, 1);
-            //fprintf(stderr, "1, j: %d, dotProds[j]: %f\n", j, dotProds[j]);
         }
         Z.getCol(i + 1, &zCol);
         for (int j = a + 1; j <= i + 1; j++) {
@@ -232,10 +221,7 @@ int arnoldiIteration(const csr_matrix& A, dense_vector& initVec, int k_total, in
                  continue;
             Z.getCol(j, &xZcol);
             dotProds[j] = cblas_ddot(counts[me], zCol, 1, xZcol, 1);
-            //fprintf(stderr, "2, j: %d, dotProds[j]: %f\n", j, dotProds[j]);
         }
-
-        fflush(stderr);
 
         MPI_Iallreduce(MPI_IN_PLACE, dotProds, i + 2, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD, &r2);
 
