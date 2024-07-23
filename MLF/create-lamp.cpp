9a9180d1b753edd6147dc287b6b55f602b1e4681
catarinab <catarina.c.bento@tecnico.ulisboa.pt>
2024-07-23 11:43:11 +0100

    create lamp matrix.

--- MLF/create-lamp.cpp
+++ MLF/create-lamp.cpp
@@ -60,6 +60,9 @@ int main (int argc, char* argv[]) {
     mkl_sparse_spmm(SPARSE_OPERATION_NON_TRANSPOSE, M.getMKLSparseMatrix(), B.getMKLSparseMatrix(),
                     A.getMKLSparseMatrixPointer());
 
+    A.printAttr();
+
+
     A.saveMatrixMarketFile((string &) "A-lamp-0.18.mtx");
 
     mkl_sparse_destroy(B.getMKLSparseMatrix());
